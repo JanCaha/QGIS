b3065052e1f0eae98009f47159e0f20927acbf03
Jan Caha <jan.caha@outlook.com>
2023-11-27 16:47:16 +0100

    drop unused

--- src/analysis/processing/qgsalgorithmscenetopoints.cpp
+++ src/analysis/processing/qgsalgorithmscenetopoints.cpp
@@ -246,110 +246,7 @@ QVariantMap QgsSceneToPointsAlgorithm::processAlgorithm( const QVariantMap &para
     return outputs;
 }
 
-std::unique_ptr< QgsAbstractGeometry > QgsSceneToPointsAlgorithm::extractTriangles(
-  const tinygltf::Model &model,
-  const tinygltf::Primitive &primitive,
-  const QgsCoordinateTransform &ecefTransform,
-  const QgsVector3D &tileTranslationEcef,
-  const QMatrix4x4 *gltfLocalTransform,
-  QgsProcessingFeedback *feedback )
-{
-  auto posIt = primitive.attributes.find( "POSITION" );
-  if ( posIt == primitive.attributes.end() )
-  {
-    feedback->reportError( QObject::tr( "Could not find POSITION attribute for primitive" ) );
-    return nullptr;
-  }
-  int positionAccessorIndex = posIt->second;
-
-  QVector< double > x;
-  QVector< double > y;
-  QVector< double > z;
-  QgsGltfUtils::accessorToMapCoordinates(
-    model, positionAccessorIndex, QgsMatrix4x4(),
-    &ecefTransform,
-    tileTranslationEcef,
-    gltfLocalTransform,
-    Qgis::Axis::Y,
-    x, y, z
-  );
-
-  std::unique_ptr< QgsMultiPolygon > mp = std::make_unique< QgsMultiPolygon >();
-
-  if ( primitive.indices == -1 )
-  {
-    Q_ASSERT( x.size() % 3 == 0 );
-
-    mp->reserve( x.size() );
-    for ( int i = 0; i < x.size(); i += 3 )
-    {
-      mp->addGeometry( new QgsPolygon( new QgsLineString( QVector<QgsPoint> { QgsPoint( x[i], y[i], z[i] ), QgsPoint( x[i + 1], y[i + 1], z[i + 1] ), QgsPoint( x[i + 2], y[i + 2 ], z[i + 2] ), QgsPoint( x[i], y[i], z[i] ) } ) ) );
-    }
-  }
-  else
-  {
-    const tinygltf::Accessor &primitiveAccessor = model.accessors[primitive.indices];
-    const tinygltf::BufferView &bvPrimitive = model.bufferViews[primitiveAccessor.bufferView];
-    const tinygltf::Buffer &bPrimitive = model.buffers[bvPrimitive.buffer];
-
-    Q_ASSERT( ( primitiveAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT
-                || primitiveAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT
-                || primitiveAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE )
-              && primitiveAccessor.type == TINYGLTF_TYPE_SCALAR );
-
-    const char *primitivePtr = reinterpret_cast< const char * >( bPrimitive.data.data() ) + bvPrimitive.byteOffset + primitiveAccessor.byteOffset;
-
-    mp->reserve( primitiveAccessor.count / 3 );
-    for ( std::size_t i = 0; i < primitiveAccessor.count / 3; i++ )
-    {
-      unsigned int index1 = 0;
-      unsigned int index2 = 0;
-      unsigned int index3 = 0;
-
-      if ( primitiveAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT )
-      {
-        const unsigned short *usPtrPrimitive = reinterpret_cast< const unsigned short * >( primitivePtr );
-        if ( bvPrimitive.byteStride )
-          primitivePtr += bvPrimitive.byteStride;
-        else
-          primitivePtr += 3 * sizeof( unsigned short );
-
-        index1 = usPtrPrimitive[0];
-        index2 = usPtrPrimitive[1];
-        index3 = usPtrPrimitive[2];
-      }
-      else if ( primitiveAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE )
-      {
-        const unsigned char *usPtrPrimitive = reinterpret_cast< const unsigned char * >( primitivePtr );
-        if ( bvPrimitive.byteStride )
-          primitivePtr += bvPrimitive.byteStride;
-        else
-          primitivePtr += 3 * sizeof( unsigned char );
-
-        index1 = usPtrPrimitive[0];
-        index2 = usPtrPrimitive[1];
-        index3 = usPtrPrimitive[2];
-      }
-      else
-      {
-        const unsigned int *uintPtrPrimitive = reinterpret_cast< const unsigned int * >( primitivePtr );
-        if ( bvPrimitive.byteStride )
-          primitivePtr += bvPrimitive.byteStride;
-        else
-          primitivePtr += 3 * sizeof( unsigned int );
-
-        index1 = uintPtrPrimitive[0];
-        index2 = uintPtrPrimitive[1];
-        index3 = uintPtrPrimitive[2];
-      }
-
-      mp->addGeometry( new QgsPolygon( new QgsLineString( QVector<QgsPoint> { QgsPoint( x[index1], y[index1], z[index1] ), QgsPoint( x[index2], y[index2], z[index2] ), QgsPoint( x[index3], y[index3], z[index3] ), QgsPoint( x[index1], y[index1], z[index1] ) } ) ) );
-    }
-  }
-  return mp;
-}
-
-QVector<QgsPoint> QgsSceneToPointsAlgorithm::renderTrianglePrimitive(
+QVector<QgsPoint> QgsSceneToPointsAlgorithm::getPolygons(
         const tinygltf::Model &model,
         const tinygltf::Primitive &primitive,
         const QgsTiledSceneTile &tile,
@@ -358,12 +255,10 @@ QVector<QgsPoint> QgsSceneToPointsAlgorithm::renderTrianglePrimitive(
         const QMatrix4x4 *gltfLocalTransform)
 {
   QVector<QgsPoint> points;
-  QVector< PrimitiveData > primitiveData;
 
   auto posIt = primitive.attributes.find( "POSITION" );
   if ( posIt == primitive.attributes.end() )
   {
-    //mErrors << QObject::tr( "Could not find POSITION attribute for primitive" );
     return points;
   }
   int positionAccessorIndex = posIt->second;
@@ -371,6 +266,7 @@ QVector<QgsPoint> QgsSceneToPointsAlgorithm::renderTrianglePrimitive(
   QVector< double > x;
   QVector< double > y;
   QVector< double > z;
+
   QgsGltfUtils::accessorToMapCoordinates(
     model, positionAccessorIndex, tile.transform() ? *tile.transform() : QgsMatrix4x4(),
     &ecefTransform,
@@ -379,175 +275,6 @@ QVector<QgsPoint> QgsSceneToPointsAlgorithm::renderTrianglePrimitive(
     static_cast< Qgis::Axis >( tile.metadata().value( QStringLiteral( "gltfUpAxis" ), static_cast< int >( Qgis::Axis::Y ) ).toInt() ),
     x, y, z
   );
-
-
-
-  //QVector< PrimitiveData > thisTileTriangleData;
-
-  if ( primitive.indices == -1 )
-  {
-    Q_ASSERT( x.size() % 3 == 0 );
-
-    points.reserve(x.size());
-    //thisTileTriangleData.reserve( x.size() );
-    for ( int i = 0; i < x.size(); i += 3 )
-    {
-      PrimitiveData data;
-      data.type = PrimitiveType::Triangle;
-      data.coordinates = QVector<QPointF> { QPointF( x[i], y[i] ), QPointF( x[i + 1], y[i + 1] ), QPointF( x[i + 2], y[i + 2] ), QPointF( x[i], y[i] ) };
-      data.z = ( z[i] + z[i + 1] + z[i + 2] ) / 3;
-
-      points.push_back(QgsPoint(x[i], y[i], z[i]));
-      points.push_back(QgsPoint(x[i+1], y[i+1], z[i+1]));
-      points.push_back(QgsPoint(x[i+2], y[i+2], z[i+2]));
-
-      //if ( needTriangle( data.coordinates ) )
-      //{
-        //thisTileTriangleData.push_back( data );
-      //}
-    }
-  }
-  else
-  {
-    const tinygltf::Accessor &primitiveAccessor = model.accessors[primitive.indices];
-    const tinygltf::BufferView &bvPrimitive = model.bufferViews[primitiveAccessor.bufferView];
-    const tinygltf::Buffer &bPrimitive = model.buffers[bvPrimitive.buffer];
-
-    Q_ASSERT( ( primitiveAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT
-                || primitiveAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT
-                || primitiveAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE )
-              && primitiveAccessor.type == TINYGLTF_TYPE_SCALAR );
-
-    const char *primitivePtr = reinterpret_cast< const char * >( bPrimitive.data.data() ) + bvPrimitive.byteOffset + primitiveAccessor.byteOffset;
-
-    //thisTileTriangleData.reserve( primitiveAccessor.count / 3 );
-    points.reserve(primitiveAccessor.count);
-
-    for ( std::size_t i = 0; i < primitiveAccessor.count / 3; i++ )
-    {
-
-      unsigned int index1 = 0;
-      unsigned int index2 = 0;
-      unsigned int index3 = 0;
-
-      PrimitiveData data;
-      data.type = PrimitiveType::Triangle;
-
-      if ( primitiveAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT )
-      {
-        const unsigned short *usPtrPrimitive = reinterpret_cast< const unsigned short * >( primitivePtr );
-        if ( bvPrimitive.byteStride )
-          primitivePtr += bvPrimitive.byteStride;
-        else
-          primitivePtr += 3 * sizeof( unsigned short );
-
-        index1 = usPtrPrimitive[0];
-        index2 = usPtrPrimitive[1];
-        index3 = usPtrPrimitive[2];
-      }
-      else if ( primitiveAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE )
-      {
-        const unsigned char *usPtrPrimitive = reinterpret_cast< const unsigned char * >( primitivePtr );
-        if ( bvPrimitive.byteStride )
-          primitivePtr += bvPrimitive.byteStride;
-        else
-          primitivePtr += 3 * sizeof( unsigned char );
-
-        index1 = usPtrPrimitive[0];
-        index2 = usPtrPrimitive[1];
-        index3 = usPtrPrimitive[2];
-      }
-      else
-      {
-        const unsigned int *uintPtrPrimitive = reinterpret_cast< const unsigned int * >( primitivePtr );
-        if ( bvPrimitive.byteStride )
-          primitivePtr += bvPrimitive.byteStride;
-        else
-          primitivePtr += 3 * sizeof( unsigned int );
-
-        index1 = uintPtrPrimitive[0];
-        index2 = uintPtrPrimitive[1];
-        index3 = uintPtrPrimitive[2];
-      }
-
-      data.coordinates = { QVector<QPointF>{ QPointF( x[index1], y[index1] ), QPointF( x[index2], y[index2] ), QPointF( x[index3], y[index3] ), QPointF( x[index1], y[index1] ) } };
-      data.z = ( z[index1] + z[index2] + z[index3] ) / 3;
-
-      points.push_back(QgsPoint(x[index1], y[index1], z[index1]));
-      points.push_back(QgsPoint(x[index2], y[index2], z[index2]));
-      points.push_back(QgsPoint(x[index3], y[index3], z[index3]));
-    }
-  }
-
-  std::sort( points.begin(), points.end(), []( const QgsPoint & a, const QgsPoint & b )
-  {
-      return a.z() < b.z();
-  } );
-
-  //primitiveData.append( thisTileTriangleData );
-  return points;
-}
-
-QgsBox3D QgsSceneToPointsAlgorithm::boxFromExtent(QgsRectangle extent, QgsCoordinateReferenceSystem extentCrs, QgsTiledSceneLayer *layer)
-{
-    const QVector< QgsVector3D > corners = QgsBox3D( extent, -10000, 10000 ).corners();
-    QVector< double > x;
-    x.reserve( 8 );
-    QVector< double > y;
-    y.reserve( 8 );
-    QVector< double > z;
-    z.reserve( 8 );
-    for ( int i = 0; i < 8; ++i )
-    {
-      const QgsVector3D &corner = corners[i];
-      x.append( corner.x() );
-      y.append( corner.y() );
-      z.append( corner.z() );
-    }
-
-    QgsCoordinateTransform transformTileToExtent = QgsCoordinateTransform( layer->dataProvider()->sceneCrs(), extentCrs, QgsCoordinateTransformContext() );
-    transformTileToExtent.transformInPlace( x, y, z, Qgis::TransformDirection::Reverse );
-
-    const auto minMaxX = std::minmax_element( x.constBegin(), x.constEnd() );
-    const auto minMaxY = std::minmax_element( y.constBegin(), y.constEnd() );
-    const auto minMaxZ = std::minmax_element( z.constBegin(), z.constEnd() );
-
-    return QgsBox3D( *minMaxX.first, *minMaxY.first, *minMaxZ.first, *minMaxX.second, *minMaxY.second, *minMaxZ.second );
-    //return QgsOrientedBox3D::fromBox3D( QgsBox3D( *minMaxX.first, *minMaxY.first, *minMaxZ.first, *minMaxX.second, *minMaxY.second, *minMaxZ.second ) );
-}
-
-QVector<QgsGeometry> QgsSceneToPointsAlgorithm::getPolygons(
-        const tinygltf::Model &model,
-        const tinygltf::Primitive &primitive,
-        const QgsTiledSceneTile &tile,
-        const QgsCoordinateTransform &ecefTransform,
-        const QgsVector3D &tileTranslationEcef,
-        const QMatrix4x4 *gltfLocalTransform)
-{
-  QVector<QgsGeometry> polygons;
-//  QVector< PrimitiveData > primitiveData;
-
-  auto posIt = primitive.attributes.find( "POSITION" );
-  if ( posIt == primitive.attributes.end() )
-  {
-    //mErrors << QObject::tr( "Could not find POSITION attribute for primitive" );
-    return polygons;
-  }
-  int positionAccessorIndex = posIt->second;
-
-  QVector< double > x;
-  QVector< double > y;
-  QVector< double > z;
-  QgsGltfUtils::accessorToMapCoordinates(
-    model, positionAccessorIndex, tile.transform() ? *tile.transform() : QgsMatrix4x4(),
-    &ecefTransform,
-    tileTranslationEcef,
-    gltfLocalTransform,
-    static_cast< Qgis::Axis >( tile.metadata().value( QStringLiteral( "gltfUpAxis" ), static_cast< int >( Qgis::Axis::Y ) ).toInt() ),
-    x, y, z
-  );
-
-
 
   QVector< PolygonWithZ > thisTileTriangleData;
 
@@ -555,8 +282,6 @@ QVector<QgsGeometry> QgsSceneToPointsAlgorithm::getPolygons(
   {
     Q_ASSERT( x.size() % 3 == 0 );
 
-    polygons.reserve(x.size());
-    //thisTileTriangleData.reserve( x.size() );
     for ( int i = 0; i < x.size(); i += 3 )
     {
         PolygonWithZ triangle;
