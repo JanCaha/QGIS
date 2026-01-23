5b208e8a937743a43763861f11abad42f464e5d4
Jan Caha <jan.caha@outlook.com>
2026-01-23 15:44:02 +0100

    add actions

--- src/app/qgsapplayertreeviewmenuprovider.cpp
+++ src/app/qgsapplayertreeviewmenuprovider.cpp
@@ -890,6 +890,10 @@ QMenu *QgsAppLayerTreeViewMenuProvider::createContextMenu()
           }
         }
 
+        menuStyleManager->addSeparator()->setObjectName( "LoadSaveStyleSeparator"_L1 );
+        menuStyleManager->addAction( tr( "Load Style…" ), app, [app, layer] { app->loadStyleFromFile( layer ); } );
+        menuStyleManager->addAction( tr( "Save Style…" ), app, [app, layer] { app->saveStyleFile( layer ); } );
+
         menu->addMenu( menuStyleManager );
       }
       else
